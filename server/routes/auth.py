a8b53aca464094d3940abc94a2e26c6fae297b3a
israel <israekbaruque992@gmail.com>
2025-03-23 21:35:28 -0300

    f: login

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,4 +1,5 @@
 from pydantic_schemas.user_create import UserCreate
+from pydantic_schemas.user_login import UserLogin
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from fastapi import APIRouter
@@ -8,6 +9,7 @@ import bcrypt
 import uuid
 
 router = APIRouter()
+
 @router.post('/signup')
 def signup_user(user: UserCreate, db: Session=Depends(get_db)):
     user_db = db.query(User).filter(User.email == user.email).first()
@@ -18,4 +20,14 @@ def signup_user(user: UserCreate, db: Session=Depends(get_db)):
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
+    return user_db
+
+@router.post('/login')
+def login_user(user: UserLogin, db: Session = Depends(get_db)):
+    user_db = db.query(User).filter(User.email == user.email).first()
+    if not user_db:
+        raise HTTPException(400, 'User com este email não existe!')
+    is_match = bcrypt.checkpw(user.password.encode(), user_db.password)
+    if not is_match:
+        raise HTTPException(400, 'senha incorreta!')
     return user_db
